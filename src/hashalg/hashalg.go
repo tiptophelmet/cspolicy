271fee88242b833cb9d00cc6ae6cf3dade468a8f
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-11 13:07:28 +0200

    👌 IMPROVE: Use interface for hash algorithm type

--- src/hashalg/hashalg.go
+++ src/hashalg/hashalg.go
@@ -1,9 +1,38 @@
 package hashalg
 
-type HashAlgorithm string
+type HashAlgorithm interface {
+	GetAlgorithmID() string
+}
 
-const (
-	SHA256 HashAlgorithm = "sha256"
-	SHA384 HashAlgorithm = "sha384"
-	SHA512 HashAlgorithm = "sha512"
-)
+type sha256 struct {
+}
+
+func (alg *sha256) GetAlgorithmID() string {
+	return "sha256"
+}
+
+func Sha256() *sha256 {
+	return &sha256{}
+}
+
+type sha384 struct {
+}
+
+func (alg *sha384) GetAlgorithmID() string {
+	return "sha384"
+}
+
+func Sha384() *sha384 {
+	return &sha384{}
+}
+
+type sha512 struct {
+}
+
+func (alg *sha512) GetAlgorithmID() string {
+	return "sha512"
+}
+
+func Sha512() *sha512 {
+	return &sha512{}
+}
