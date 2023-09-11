d926024c04611e7dd71c92c481f49a241ad14be9
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-11 13:36:45 +0200

    👌 IMPROVE: Check if nonce base64 is empty

--- src/nonceBase64.go
+++ src/nonceBase64.go
@@ -1,15 +1,25 @@
 package src
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type NonceBase64Val struct {
 	base64 string
 }
 
 func (v *NonceBase64Val) String() string {
+	prefix := "cspolicy.src.NonceBase64"
+
+	if len(v.base64) == 0 {
+		fmt.Printf("[%s] provided base64 string is empty, skipping...\n", prefix)
+		return ""
+	}
+
 	return fmt.Sprintf("'nonce-%s'", v.base64)
 }
 
 func NonceBase64(base64 string) *NonceBase64Val {
-	return &NonceBase64Val{base64}
+	return &NonceBase64Val{strings.TrimSpace(base64)}
 }
