c39b124e7793809c972785340204f3fe95a50850
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-12 19:22:15 +0200

    📖 DOC: Document cspolicy package

--- cspolicy.go
+++ cspolicy.go
@@ -1,9 +1,14 @@
+// Content-Security-Policy package.
+// Provides functions to produce a valid Content-Security-Policy HTTP header value.
 package cspolicy
 
 import (
 	"strings"
 )
 
+// Builds Content-Security-Policy.
+// 
+// Accepts a slice of directives, formats them and returns a CSP header value.
 func Build(directives ...string) string {
 	if len(directives) == 0 {
 		return ""
